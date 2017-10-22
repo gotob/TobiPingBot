f414b95b9288fb51c2e2b067ac7076407d79e38d
Tobia Poppi <tobia.poppi@virgilio.it>
2017-10-22 11:43:34 +0200

    working with json file
    Now the program launch automatically a number of threads equal to the number of ip address of the json file, and it continuously send their status.
    Also keyboard interrupt + enter working.
    Next step: datetime.

--- pingTobiBot.py
+++ pingTobiBot.py
@@ -3,7 +3,9 @@ import telepot
 import json
 import subprocess
 import re
-#threads=[]
+import sys
+
+listaThreads=[]
 tobiBot = telepot.Bot('365404534:AAH4ZGos10NqOcYid4SgTp3iotqNgzTNOcg')
 fileUtenti = open('jason.json').read()
 users = json.loads(fileUtenti)
@@ -14,19 +16,30 @@ class pingThread(threading.Thread):
 		threading.Thread.__init__(self)
 		self.chatID = cid
 		self.ip = addr
-		self.attivo=True
+		self.vivo = True
 	def run(self):
-		while (self.attivo):
+		while (self.vivo):
 			ping = 'ping -n 2 ' + self.ip
 			(out) = subprocess.Popen(ping, stdout=subprocess.PIPE, shell=True).communicate()
 			if re.search('Ricevuti = 0', str(out)):
-				print str(out)
 				tobiBot.sendMessage(self.chatID, str(self.ip) + ' offline')
 			else:
 				tobiBot.sendMessage(self.chatID, str(self.ip) + ' online')
 
-
 #estrai il dizionario
 #per ogni ip dell'user crei un thread
-t1 = pingThread('164433105', '192.168.100.1')
-t1.start()
+for key in users:
+	a = users[key]
+	for kkey in a:
+		for ips in a[kkey]:
+			print ips
+			t1 = pingThread(str(key), str(ips))
+			listaThreads.append(t1)
+			t1.start()
+
+ki = ''
+#while ki == '':
+for t in listaThreads:
+	ki = sys.stdin.read(1)
+	t.vivo = False
+	t.join()
